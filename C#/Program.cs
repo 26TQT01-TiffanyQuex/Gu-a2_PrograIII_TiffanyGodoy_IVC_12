dc01f6f8bbaf8f967e4b8710da9e0d7accd148ff
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-07-05 16:58:54 -0600

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Colegio Salesiano Don Bosco\nTiffany Alejandra Quex\nIV Bach sección C\nClave 12");
+        int[,] columnas = new int[5, 5];
+
+        //Recorrer cada fila
+        for (int fila = 1; fila < 5; fila++)
+        {
+            for (int columna = 1; columna < 5; columna++)
+            {
+                columnas[fila, columna] = columna;
+            }
+        }
+
+        //Mostrar la matriz llena
+        for (int fila = 1; fila < 5; fila++)
+        {
+            for (int columna = 1; columna < 5; columna++)
+            {
+                Console.Write(columnas[fila,columna]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
